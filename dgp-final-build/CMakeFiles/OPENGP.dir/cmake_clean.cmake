944de5e8775745398e97fcc9cdb7dcc69a84ec71
Evan Nickerson <evannick@ecs354-10.csc.uvic.ca>
2016-04-06 17:45:31 -0700

    updated repo to new .gitignore

--- dgp-final-build/CMakeFiles/OPENGP.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/opengp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/opengp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
